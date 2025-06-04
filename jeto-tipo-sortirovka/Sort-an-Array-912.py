570decd4b6eb60ec86b478a7ba5848103ac8a505
Yehor-Holovatyi <maizon200725@gmail.com>
2025-06-04 19:52:15 +0300

    finish 912

--- jeto-tipo-sortirovka/Sort-an-Array-912.py
+++ jeto-tipo-sortirovka/Sort-an-Array-912.py
@@ -1,2 +1,17 @@
-class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
+class Solution: # copy of LeetCode task number(912) name(Sort an Array)
+    def sortArray(self, nums: List[int]) -> List[int]: # copy of LeetCode task number(1050) name(height checkers)
+       
+        # (nums) list of numbers ;  (sorted(nums)) sorts numbers
+
+        result = sorted (nums)
+
+        # (result) we save sorted list: nums [3 1 5 4 2 1 5] 
+        
+        # result = sorted (nums) 
+        
+        # print result (nums)= [1 1 2 3 4 5 5]
+        
+        return result
+
+
+
