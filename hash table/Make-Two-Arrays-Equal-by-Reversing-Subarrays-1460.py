faddd370c454bfb472adbdb729b06635e39aae04
Yehor-Holovatyi <maizon200725@gmail.com>
2025-06-04 23:31:09 +0300

    finish

--- hash table/Make-Two-Arrays-Equal-by-Reversing-Subarrays-1460.py
+++ hash table/Make-Two-Arrays-Equal-by-Reversing-Subarrays-1460.py
@@ -1,3 +1,15 @@
 class Solution:
      def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
         # i have target and arr, i have task if they match then it's true, if not then it's false 
+
+        target_sorted=sorted(target)
+
+        arr_sorted=sorted(arr)
+        #sorts the numbers in this way [1 2 3 4 5 6]
+
+        if target_sorted==arr_sorted:
+        # if clones write true
+            return True
+        else:
+            return False
+        
