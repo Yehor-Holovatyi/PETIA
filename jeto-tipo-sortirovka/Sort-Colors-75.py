95654ca671ee8d6d1107624bb04ce3c6ede0d93c
Yehor-Holovatyi <maizon200725@gmail.com>
2025-06-04 20:07:38 +0300

    finish

--- jeto-tipo-sortirovka/Sort-Colors-75.py
+++ jeto-tipo-sortirovka/Sort-Colors-75.py
@@ -3,4 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        nums.sort()
+
+        # (nums.sort()) the command rearranges the numbers in order: num [1 4 0 3 2]; nums.sort() [0 1 2 3 4]
